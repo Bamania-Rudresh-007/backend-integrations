9037b54db454f8a0ad645838319b476913002ecb
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-15 19:23:33 +0530

    add routes for all endpoints

---
+++ routes/users.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { readUsers, readUserById, createUser, updateUserById, delteUserById } from "../controllers/users.controller.js"
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.end("hey there welcome to the implementation of the basic methods of http and mongoDb database crub operations");
+})
+
+router.get("/users", readUsers);
+router.get("/users/:id", readUserById);
+router.post("/users/data", createUser);
+router.put("/users/:id", updateUserById);
+router.delete("/users/:id", delteUserById);
+
+export default router;
