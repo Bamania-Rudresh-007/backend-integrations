5008cf3fd9e35270c0bce0d6746bf107293026c5
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-18 18:37:07 +0530

    Added protect middleware which confirms the token before login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 import expres from "express";
 import { signUp, getAuthUsers, login } from "../controllers/auth.controller.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const authRoutes = expres.Router()
 
 
 
-authRoutes.get("/get", getAuthUsers);
+authRoutes.get("/get", protect,getAuthUsers);
 authRoutes.post("/signup", signUp);
 authRoutes.post("/login", login);
 
